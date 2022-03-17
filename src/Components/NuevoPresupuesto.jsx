47016cdc9350bac5f48ffccfe6d3f330d864458f
pipeswk <afco1995@gmail.com>
2022-03-16 20:01:21 -0500

    Nuevo componente

---
+++ src/Components/NuevoPresupuesto.jsx
@@ -0,0 +1,46 @@
+import { useState } from "react";
+import Mensaje from "./Mensaje";
+
+
+const NuevoPresupuesto = ( { presupuesto, setPresupuesto } ) => {
+
+    const [ mensaje, setMensaje ] = useState('');
+
+    const handlePresupuesto = (e) => {
+        e.preventDefault();
+
+        if(presupuesto <= 0) {
+            setMensaje('Presupuesto invalido')
+        } else {
+            setMensaje('');
+        }
+    }
+
+  return (
+    <div className="contenedor-presupuesto contenedor sombra">
+        <form className="formulario" onSubmit={handlePresupuesto}>
+            <div className="campo">
+                <label>Definir presupuesto</label>
+                <input
+                className="nuevo-presupuesto"
+                type="number"
+                // min="0"
+                placeholder={`$ Agrega tu presupuesto mensual`}
+                onChange={ e => setPresupuesto(Number(e.target.value)) }
+                />
+            </div>
+
+            <input
+            type="submit"
+            value='Añadir' 
+            />
+
+            {mensaje && <Mensaje tipo="error">{mensaje}</Mensaje>}
+
+
+        </form>
+    </div>
+  )
+}
+
+export default NuevoPresupuesto
