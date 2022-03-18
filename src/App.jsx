0abef1d5d9569d57a53b16714df9b4f580e4a7d9
pipeswk <afco1995@gmail.com>
2022-03-17 22:48:40 -0500

    Se agregan componentes Modal y sus funciones

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,52 @@
 import { useState } from 'react'
 import Header from './Components/Header'
+import Modal from './Components/Modal';
+import iconoNuevoGasto from './img/nuevo-gasto.svg'
 
 function App() {
   const [ presupuesto, setPresupuesto ] = useState(0);
+  const [ validarPresupuesto, setValidarPresupuesto ] = useState(false);
+  const [ modal, setModal ] = useState(false);
+  const [ mostrarFormModal, setMostrarFormModal ] = useState(false);
+
+
+  const handleNuevoGasto = () => {
+    setModal(true);
+
+    setTimeout(() => {
+      console.log('Activado')
+      setMostrarFormModal(true)
+    }, 600);
+
+  }
 
   return (
     <div>
       <Header
         presupuesto={presupuesto}
         setPresupuesto={setPresupuesto}
+        validarPresupuesto={validarPresupuesto}
+        setValidarPresupuesto={setValidarPresupuesto}
       />
+
+      {validarPresupuesto === true ? (
+        <div className='nuevo-gasto'>
+          <img
+            src={iconoNuevoGasto}
+            alt="icono nuevo gasto"
+            onClick={handleNuevoGasto}
+          />
+        </div>
+      ) : null}
+
+      {modal === true ? (
+        <Modal
+          setModal={setModal}
+          setMostrarFormModal={setMostrarFormModal}
+          mostrarFormModal={mostrarFormModal}
+        />
+      ) : null }
+
     </div>
   )
 }
