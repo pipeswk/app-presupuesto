78b7325d930dff68480d3ecb259035ad5948bc68
pipeswk <afco1995@gmail.com>
2022-03-22 18:21:48 -0500

    Se agrega condicional para mostrar gastos filtrados

--- src/Components/ListadoGastos.jsx
+++ src/Components/ListadoGastos.jsx
@@ -1,18 +1,30 @@
 import Gasto from "./Gasto"
 
-const ListadoGastos = ( { gastos, setEditarGasto, eliminarGasto } ) => {
+const ListadoGastos = ( { gastos, setEditarGasto, eliminarGasto, gastosFiltrados, filtro } ) => {
+
   return (
     <div className="listado-gastos contenedor">
-        <h2>{gastos.length ? 'Gastos' : 'No Hay Gastos aun'}</h2>
+      <h2>{gastos.length ? 'Gastos' : 'No Hay Gastos aun'}</h2>
 
-        {gastos.map( gasto => (      // el .map recorre cada elemento del array y crea una carta para cada gasto segun la cantidad
-            <Gasto
-            key={gasto.id}
-            gasto={gasto}
-            setEditarGasto={setEditarGasto}
-            eliminarGasto={eliminarGasto}
-            />
-        ) )}
+      {filtro ? (
+        gastosFiltrados.map( gasto => (      // el .map recorre cada elemento del array y crea una carta para cada gasto segun la cantidad
+          <Gasto
+          key={gasto.id}
+          gasto={gasto}
+          setEditarGasto={setEditarGasto}
+          eliminarGasto={eliminarGasto}
+          />
+        ) )
+      ) : (
+        gastos.map( gasto => (      // el .map recorre cada elemento del array y crea una carta para cada gasto segun la cantidad
+          <Gasto
+          key={gasto.id}
+          gasto={gasto}
+          setEditarGasto={setEditarGasto}
+          eliminarGasto={eliminarGasto}
+          />
+        ) )
+      )}
 
     </div>
   )
